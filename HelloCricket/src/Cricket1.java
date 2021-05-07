cd0b1b88268099bfecf32d2cfc8ae201a51b0a2d
ranjit kumar <ranjitkumart@gmail.com>
2021-05-07 11:56:49 +0530

    this is commit from Asian Timezone guy

--- HelloCricket/src/Cricket1.java
+++ HelloCricket/src/Cricket1.java
@@ -4,7 +4,7 @@ public class Cricket1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is cricket1 program");
+		System.out.println("This is cricket1 program and this has been updated now");
 
 	}
 
---
+++ HelloCricket/src/Cricket3.java
@@ -0,0 +1,10 @@
+
+public class Cricket3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("A new cricket3 file has been created");
+	}
+
+}
